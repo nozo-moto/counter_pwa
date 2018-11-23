deefbd9bc602d7318eb7b0b3a1fbc602aa3e6448
nozomi0966 <nozomotoitech@gmail.com>
2018-11-23 14:20:48 +0900

    add vuetify

---
+++ src/components/counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="counter">
+        <button></button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "counter",
+  data() {
+    return {
+      count: 0
+    };
+  }
+};
+</script>
+
+<style>
+</style>
